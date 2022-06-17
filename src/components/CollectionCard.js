8a50cfaf12a7bc41727d35d1852527010539b23b
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-17 14:42:40 +0530

    card layout

---
+++ src/components/CollectionCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CollectionCard = (id, name, traits, image) => {
+  return (
+    <div className='collectionCard'>
+        <img src={image} alt="" />
+        <div className='details'></div>
+        <div className='name'>
+            {name} <div className='id'> 🍁#{id} </div>
+        </div>
+        <div className='priceContainer'>
+            <img src={weth} className='wethImage' alt="" />
+            <div className='price'>{traits[0]?.value}</div>
+        </div>
+    </div>
+  )
+}
+
+export default CollectionCard
