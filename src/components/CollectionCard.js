a5d17d4249e2afaf3fbfe4a486afa7ee1f57d1a2
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-17 15:34:34 +0530

    import style

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import weth from '../assets/weth.png'
+import './CollectionCard.css'
 
 const CollectionCard = ({id, name, traits, image}) => {
   return (
     <div className='collectionCard'>
-        <img src={image} alt="" />
-        <div className='details'></div>
-        <div className='name'>
-            {name} <div className='id'> 🍁#{id} </div>
-        </div>
+        <img src={image} />
+        <div className='details'>
+          <div className='name'>
+              {name} <div className='id'> 🍁#{id} </div>
+          </div>        
         <div className='priceContainer'>
-            <img src={weth} className='wethImage' alt="" />
+            <img src={weth} className='wethImage' />
             <div className='price'>{traits[0]?.value}</div>
         </div>
+      </div>
     </div>
   );
 }
