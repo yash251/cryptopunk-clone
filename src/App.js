701a6fe90b0f777b5694b2c59620927e851ef72c
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-17 19:07:23 +0530

    axios to read nft data from opensea

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import './App.css';
 import Header from './components/Header';
 import CollectionCard from './components/CollectionCard'
 import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
+
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        'https://testnets-api.opensea.io/assets?asset_contract_address=0x07dEe7a6db902717235C78f53764AAEfECABfA74&order_direction=asc'
+      )
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets)
+    }
+
+    getMyNfts();    
+  }, []);
+  
   return (
     <div className='app'>
       <Header />
