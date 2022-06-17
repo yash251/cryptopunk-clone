43d10247ecdddf41db1bc23c568b70fb6ce1fa41
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-17 15:16:04 +0530

    CollectionCard render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard'
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <CollectionCard 
+        id={0} 
+        name={'King Punk'} 
+        traits={[{ value : 7 }]} 
+        image='https://i.imgur.com/ARwnIqd.jpeg' 
+      />
     </div>
   );
 }
