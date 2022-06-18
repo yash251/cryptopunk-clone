d0d8455fada484d23556066e0c4025935e6cdc93
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-18 10:15:16 +0530

    removed hardcoded component

--- src/App.js
+++ src/App.js
@@ -24,12 +24,6 @@ function App() {
   return (
     <div className='app'>
       <Header />
-      <CollectionCard 
-        id={0} 
-        name={'King Punk'} 
-        traits={[{ value : 7 }]} 
-        image='https://i.imgur.com/ARwnIqd.jpeg' 
-      />
       <PunkList punkListData={punkListData} />
     </div>
   );
